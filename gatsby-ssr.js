6b616a4fa4712a0e2608444a785e52e4744f5baf
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-05-06 23:08:38 -0600

    added plausible

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,12 @@
-export const onRenderBody = ({ setHeadComponents }) => {
+import React from "react"
+export const onRenderBody = (
+  { setHeadComponents, setPostBodyComponents },
+  pluginOptions
+) => {
   setHeadComponents([
+    <script async defer data-domain="links.sarasana.studio" src="https://plausible.io/js/plausible.js"></script>
+  ])
+  setPostBodyComponents([
     <script async defer data-domain="links.sarasana.studio" src="https://plausible.io/js/plausible.js"></script>
   ])
 }
