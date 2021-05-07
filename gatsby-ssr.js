7da902841e4c40ec60490883e371985872c9132d
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-05-06 22:58:59 -0600

    added plausible analytics

---
+++ gatsby-ssr.js
@@ -0,0 +1,5 @@
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script async defer data-domain="links.sarasana.studio" src="https://plausible.io/js/plausible.js"></script>
+  ])
+}
